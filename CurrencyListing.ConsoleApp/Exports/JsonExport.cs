0f1231b48d14ce75ba1483bd69fd101503061716
h.sametdemir <h.sametdemir@hotmail.com>
2021-12-07 23:26:49 +0300

    ConsoleApp project insterted.

---
+++ CurrencyListing.ConsoleApp/Exports/JsonExport.cs
@@ -0,0 +1,25 @@
+﻿using CurrencyListing.Exports.Abstract;
+using CurrencyListing.Models.Base;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CurrencyListing.ConsoleApp.Exports
+{
+    public class JsonExport : ICurrencyExport
+    {
+        public FileResult Convert(IEnumerable<CurrencyRate> list)
+        {
+            var fileResult = new FileResult { Extension = "json" };
+            using var memoryStream = new MemoryStream();
+            using (TextWriter streamWriter = new StreamWriter(memoryStream))
+            {
+                streamWriter.Write(JsonConvert.SerializeObject(list));
+            }
+
+            fileResult.FileData = memoryStream.ToArray();
+            return fileResult;
+        }
+    }
+}
---
+++ CurrencyListing.ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using CurrencyListing.ConsoleApp.Exports;
+using CurrencyListing.Helpers;
+using CurrencyListing.Integrations.Concrete;
+using CurrencyListing.Models.Enums;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace CurrencyListing.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currencyListingHelper = new CurrencyListingHelper();
+            List(currencyListingHelper);
+            Export(currencyListingHelper);     
+        }
+
+        private static void List(CurrencyListingHelper currencyListingHelper)
+        {
+            var listResponse = currencyListingHelper.List(new TcmbIntegration(), sortField: SortField.ForexBuying, sortDirection: SortDirection.Descending);
+
+            if (listResponse.IsDone &&
+                listResponse.Data != null &&
+                listResponse.Data.Any())
+            {
+                Console.WriteLine("Currency | Forex buying | Forex Selling | Banknote buying | Banknote Selling");
+                foreach (var currency in listResponse.Data)
+                {
+                    Console.WriteLine($"{currency.NameTr} ({currency.CurrencyCode}) | {currency.ForexBuying} | {currency.ForexSelling} | {currency.BanknoteBuying} | {currency.BanknoteSelling}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Export(CurrencyListingHelper currencyListingHelper)
+        {
+            var exportResponse = currencyListingHelper.Export(new TcmbIntegration(), new JsonExport());
+
+            if (exportResponse.IsDone &&
+                exportResponse.Data != null &&
+                exportResponse.Data.FileData != null)
+            {
+                var fileResult = exportResponse.Data;
+                var rootFolder = "C:\\exports";
+                var subFolder = $"{rootFolder}\\{exportResponse.Data.Extension}";
+                if (!Directory.Exists(rootFolder)) Directory.CreateDirectory(rootFolder);
+                if (!Directory.Exists(subFolder)) Directory.CreateDirectory(subFolder);
+
+                File.WriteAllBytes($"C:\\exports\\{fileResult.Extension}\\data.{fileResult.Extension}", fileResult.FileData);
+
+            }
+        }
+    }
+}
